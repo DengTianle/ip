691ea79a798bef30aeb8dda63154b611896a67de
DengTianle <dengtianle51@gmail.com>
2025-01-22 12:35:57 +0800

    Rename, Greet, Exit.

---
+++ src/main/java/Zazu.java
@@ -0,0 +1,14 @@
+public class Zazu {
+    public static void main(String[] args) {
+        String logo =
+            "  ZZZZZ   AAAAA   ZZZZZ   U   U\n" +
+            "    Z    A     A     Z    U   U\n" +
+            "   Z     AAAAAAA    Z     U   U\n" +
+            "  Z      A     A   Z      U   U\n" +
+            " ZZZZZ   A     A  ZZZZZ   UUUUU";
+
+        System.out.println("Hello! I'm \n" + logo);
+        System.out.println("What can I do for you?\n");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
