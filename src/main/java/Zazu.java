933512410ba22965b0d75487cf561972f7267791
DengTianle <dengtianle51@gmail.com>
2025-01-22 13:05:55 +0800

    Echo.

--- src/main/java/Zazu.java
+++ src/main/java/Zazu.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Zazu {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
         String logo =
             "  ZZZZZ   AAAAA   ZZZZZ   U   U\n" +
             "    Z    A     A     Z    U   U\n" +
@@ -9,6 +13,15 @@ public class Zazu {
 
         System.out.println("Hello! I'm \n" + logo);
         System.out.println("What can I do for you?\n");
+
+        while (true) {
+            String str = input.nextLine();
+            if (str.equals("bye")) {
+                break;
+            }
+            System.out.println(str);
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
